c34147d486ca8a6d7aaa9fa3d658cc3711120894
thomas ledesert <thomas.ledesert@gmail.com>
2020-09-29 20:36:51 +0200

    wip

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,33 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    
-  </div>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Todos</h1>
+      <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model="newTodo" @keyup.enter="addTodo">
+    </header>
+    <div class="main">
+      <input type="checkbox" class="toggle-all" v-model="allDone">
+      <ul class="todo-list">
+        <li class="todo" v-for="todo in todos" v-bind:key="todo">
+          <div class="view">
+             <input type="checkbox" v-model="todo.completed" class="toggle">
+            <label @dblclick="editTodo(todo)">{{ todo.name }}</label>
+            <button class="destroy" @click.prevent="deleteTodo(todo)"></button>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <!--
+    <footer class="footer" v-show="hasTodos">
+      <span class="todo-count"><strong>{{ remaining }}</strong> tâches à faire</span>
+      <ul class="filters">
+        <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes</a></li>
+        <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire</a></li>
+        <li><a href="#" :class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
+      </ul>
+      <button class="clear-completed" v-show="completed" @click.prevent="deleteCompleted">Supprimer les tâches finies</button>
+    </footer>
+    -->
+  </section>
 </template>
 
 <script>
 export default {
   name: 'TodoList',
-  props: {
-    msg: String
+  data () {
+    return {
+      todos: [
+        {
+          name : 'premiere tache',
+          completed : false
+        },
+        {
+          name : 'seconde tache',
+          completed : true
+        }
+
+      ],
+      newTodo: '',
+      filter: 'all',
+      editing: null,
+      oldTodo: ''
+    }
+  },
+  methods: {
+    addTodo(){
+      this.todos.push({
+        completed:false,
+        name : this.newTodo
+      })
+      this.newTodo = '' 
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
+<style src="./app.css"></style>
